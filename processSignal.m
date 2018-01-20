a7ac0348d36d4328fea833ebfcfd353e6d77d82b
ziele3920 <a.ziolkowski3920@wp.pl>
2018-01-20 18:46:43 +0100

    plotting different filters

--- processSignal.m
+++ processSignal.m
@@ -1,5 +1,5 @@
 close all; clear; clc;
-currentFileName = 'GT_N_03_04.txt';
+currentFileName = 'GT_F_05_02.txt';
 [TS,Resp,BP,ECG] = readFile(['data/' currentFileName]);
 signalData.fileName = currentFileName;
 signalData.time = TS;
@@ -7,41 +7,48 @@ signalData.resp = Resp;
 signalData.BP = BP;
 signalData.ECG = ECG;
 
-cutoffTimeInSec = 30;
+cutoffTimeInSec = 25;
 samplingFreq = 100;
 signalData.BP = signalData.BP(1:cutoffTimeInSec*samplingFreq);
 signalData.time = signalData.time(1:cutoffTimeInSec*samplingFreq);
 
-fs = 200;
-t = 0:1/fs:max(signalData.time);
-signalData.BP = interp1(signalData.time, signalData.BP, t);
-signalData.time = t;
+fs = 100;
+%t = 0:1/fs:max(signalData.time);
+%signalData.BP = interp1(signalData.time, signalData.BP, t);
+%signalData.time = t;
 
 %detrend and normalize
 detrendedSignal = detrend(signalData.BP);
 detrendedSignal=detrendedSignal./max(abs(detrendedSignal));
 
-lpFilter = designfilt('lowpassiir','FilterOrder',8, ...
+lpFilter = designfilt('lowpassiir','FilterOrder',2, ...
          'PassbandFrequency', 6,'PassbandRipple',0.2, ...
          'SampleRate',fs);
 %fvtool(lpFilter)
 
-hpFilter = designfilt('highpassiir','FilterOrder',8, ...
-         'PassbandFrequency',0.8,'PassbandRipple',0.2, ...
+hpFilter = designfilt('highpassiir','FilterOrder',2, ...
+         'PassbandFrequency',0.4,'PassbandRipple',0.2, ...
          'SampleRate',fs);
-fvtool(hpFilter)
+%fvtool(hpFilter)
 
 
 lpFiltered = filter(lpFilter, detrendedSignal);
 hpFiltered = filter(hpFilter, lpFiltered);
+hpFilteredO = filter(hpFilter, detrendedSignal);
 
 figure;
-subplot(311);
+subplot(211);
 plot(signalData.time, signalData.BP);
 hold on;
-subplot(312);
+subplot(212);
 plot(signalData.time, detrendedSignal);
 hold on;
-subplot(313);
-plot(signalData.time, hpFiltered);
+subplot(212);
+plot(signalData.time, lpFiltered, 'r');
+hold on;
+subplot(212);
+plot(signalData.time, hpFiltered, 'g');
+hold on;
+subplot(212);
+plot(signalData.time, hpFilteredO, 'm');
 
